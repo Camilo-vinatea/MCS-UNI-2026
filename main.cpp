79822022180f3f92ca498f2e3b4aeccc614dbce3
Camilo <camo2391@gmail.com>
2026-02-06 20:34:59 -0500

    commit antes de rama 7

--- main.cpp
+++ main.cpp
@@ -35,10 +35,11 @@ int main(){
 
 // 3. Diaz Tapia Adderly
 // substraction
+    {
     std::cout << "Demo Adderly Díaz Tapia" << std::endl;
     Ts d1 = 10.1; d2=7.2;
     std::cout<<"La resta es: " << subtraction(d1, d2) << "\n\n";
-
+    }
 // 4. Lopez Flores Royer Amed
 // multiplication
     std::cout << "Demo Lopez Flores Royer Amed" << std::endl;
